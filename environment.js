57be03144bc5b6736ef90c148cb171df8de0754d
Malvin Alexious <c14220245@john.petra.ac.id>
2024-05-31 16:37:06 +0700

    update:fbxloader add:rsrc

--- environment.js
+++ environment.js
@@ -1,27 +1,76 @@
 import * as THREE from "three";
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import {FBXLoader} from 'three/addons/loaders/FBXLoader.js';
 
 export class obj {
-    constructor(filepath, x, y, z, scale, rotx, roy, rotz) {
-        this.filepath = filepath;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.scale = scale;
-        this.rotx = rotx;
-        this.roy = roy;
-        this.rotz = rotz;
-    };
+    constructor(path, OBJ, MTL, radX, radY, radZ, x, y, z, rotX, rotY, rotZ, sceneFromMain){
+        this.loadObj(path, OBJ, MTL, radX, radY, radZ, x, y, z, rotX, rotY, rotZ, sceneFromMain);
+    }
+    loadObj(path, OBJ, MTL, radX, radY, radZ, x, y, z, rotX, rotY, rotZ, sceneFromMain) {
+        //Object
+        const onProgress = function (xhr) {
 
-    loadModel(filepath, x, y, z, scale, rotx, roy, rotz) {
-        this.mesh = new THREE.Mesh(
-            new new OBJLoader().load(filepath)
-        );
-        this.scene.add(this.mesh);
-        this.mesh.castShadow = true;
-        this.mesh.receiveShadow = true;
-        this.mesh.position.set(x, y, z);
-        this.mesh.scale.set(scale, scale, scale);
-        this.mesh.rotation.set(rotx, roy, rotz);
+            if (xhr.lengthComputable) {
+
+                const percentComplete = xhr.loaded / xhr.total * 100;
+                console.log(percentComplete.toFixed(2) + '% downloaded');
+
+            }
+
+        };
+        new MTLLoader()
+            .setPath(path)
+            .load(MTL, function (materials) {
+
+                materials.preload();
+
+                new OBJLoader()
+                    .setMaterials(materials)
+                    .setPath(path)
+                    .load(OBJ, function (object) {
+
+                        sceneFromMain.add(object);
+                        object.castShadow = true;
+                        object.receiveShadow = true;
+                        object.scale.set(radX, radY, radZ);
+                        object.position.set(x, y, z);
+                        object.rotation.x = rotX;
+                        object.rotation.y = rotY;
+                        object.rotation.z = rotZ;
+
+                    }, onProgress);
+
+            });
+    }
+    
+}
+
+export class fbx{
+    constructor(path, FBXFile, radX, radY, radZ, x, y, z, rotX, rotY, rotZ, sceneFromMain){
+        this.loadModel(path, FBXFile, radX, radY, radZ, x, y, z, rotX, rotY, rotZ, sceneFromMain);
+    }
+    
+    loadModel(path, FBXFile, radX, radY, radZ, x, y, z, rotX, rotY, rotZ, sceneFromMain){
+        var loader = new FBXLoader();
+        loader.setPath(path);
+        loader.load(FBXFile, (fbx) => {
+            fbx.scale.setScalar(0.01);
+            fbx.traverse(c => {
+              c.castShadow = true;
+              c.receiveShadow = true;
+              c.scale.set(radX, radY, radZ);
+              c.position.set(x, y, z);
+              c.rotation.x = rotX;
+              c.rotation.y = rotY;
+              c.rotation.z = rotZ;
+            });
+            this.mesh = fbx;
+            sceneFromMain.add(this.mesh);
+            this.mesh.rotation.y += Math.PI/2;
+
+            this.mixer = new THREE.AnimationMixer(this.mesh);            
+        });
+
     }
 }
--- main.js
+++ main.js
@@ -1,40 +1,41 @@
 import * as THREE from "three";
 import { Player, PlayerController, ThirdPersonCamera } from "./player.js";
+import { fbx, obj } from "./environment.js";
 
 
-class Main{
-    static WindowResize(){
+class Main {
+    static WindowResize() {
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
-    static init(){
+    static init() {
         var canvReference = document.getElementById("canvas");
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.renderer = new THREE.WebGLRenderer({antialias: true, canvas:canvReference});
+        this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvReference });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.setClearColor(0x000000, 1);
         this.renderer.shadowMap.enabled = true;
 
         window.addEventListener('resize', () => {
             Main.WindowResize();
-          }, false);
+        }, false);
 
         //Plane
-        var plane = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: 0xcbcbcb } ) );
+        var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshPhongMaterial({ color: 0x00ff00 }));
         plane.rotation.x = - Math.PI / 2;
         plane.receiveShadow = true;
         plane.castShadow = true;
-        this.scene.add( plane );
+        this.scene.add(plane);
 
         //Ambient Light
-        var ambientLight = new THREE.AmbientLight(0x8888FF,1);
+        var ambientLight = new THREE.AmbientLight(0xDDEEFF, 0.75);
         this.scene.add(ambientLight);
 
         //Directional Light
         var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-        directionalLight.position.set( 3, 10, 10 );
+        directionalLight.position.set(3, 10, 10);
         directionalLight.castShadow = true;
         directionalLight.shadow.camera.top = 20;
         directionalLight.shadow.camera.bottom = -20;
@@ -52,25 +53,28 @@ class Main{
         // ThirdPersonCamera
         this.player = new Player(
             new ThirdPersonCamera(
-                this.camera, new THREE.Vector3(-3,1,0), new THREE.Vector3(0,1,0)
+                this.camera, new THREE.Vector3(-5, 2, 0), new THREE.Vector3(0, 0, 0)
             ),
             new PlayerController(),
             this.scene,
             10
         );
 
-        //Object
-        this.mesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1,1,1),
-            new THREE.MeshPhongMaterial({color: 0xFFFF11})
-        );
-        this.scene.add(this.mesh);
-        this.mesh.castShadow = true;
-        this.mesh.receiveShadow = true;
-        this.mesh.position.set(3,0,0);
+
+
+
+
+
+
+        // var tree = new obj('./non-player asset/Park2/acaciaTree.obj', 0, 0, 0, 1, 0, 0, 0);
+        // this.scene.add(tree.mesh);
+        // this.scene.add(this.mesh);
+        // this.mesh.castShadow = true;
+        // this.mesh.receiveShadow = true;
+        // this.mesh.position.set(3,0,0);
 
     }
-    static render(dt){
+    static render(dt) {
         this.player.update(dt);
         this.renderer.render(this.scene, this.camera);
     }
@@ -78,7 +82,36 @@ class Main{
 
 var clock = new THREE.Clock();
 Main.init();
-function animate(){
+
+var kursi = new obj('./non-player asset/Park2/', 'bench_res.obj', 'bench_res.mtl', 1, 1, 1, 0, 0, 0, 0, -4.65, 0, Main.scene);
+var kursi = new obj('./non-player asset/Park2/', 'bench_res.obj', 'bench_res.mtl', 1, 1, 1, 5, 0, 0, 0, 4.65, 0, Main.scene);
+var pohon = new obj('./non-player asset/Park2/', 'acaciaTree.obj', 'acaciaTree.mtl', 0.01, 0.01, 0.01, 2.5, 0, 10, 0, 0, 0, Main.scene);
+var ayunan = new obj('./non-player asset/Park2/', 'tire swing.obj', 'tire swing.mtl', 0.2, 0.2, 0.2, 2.5, 0, 0, 0, 4.67, 0, Main.scene);
+// var bambu = new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, 0, 0, 0, 0, 0, 0, Main.scene);
+// var bambu1 = new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, 0, 0, 0.5, 0, 0, 0, Main.scene);
+// var bambu2 = new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, 0, 0, -0.5, 0, 0, 0, Main.scene);
+// var bambu3 = new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, 0, 0, 0.25, 0, 0, 0, Main.scene);
+// var bambu4 = new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, 0, 0, -0.25, 0, 0, 0, Main.scene);
+// var bambu5 = new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, 0, 0, 0.75, 0, 0, 0, Main.scene);
+// var bambu6 = new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, 0, 0, -0.75, 0, 0, 0, Main.scene);
+
+var vertices = [];
+for (let i = 0; i < 360; i++) {
+    var angleInRadians = (i * Math.PI) / 180;
+    var newX = 0 + Math.cos(angleInRadians) * 5; // X-coordinate remains the same
+    var newY = 0; // Rotate around X-axis
+    var newZ = 0 + Math.sin(angleInRadians) * 5; // Translate along Z-axis
+    vertices.push(newX);
+    vertices.push(newY);
+    vertices.push(newZ);
+}
+for (let i = 0; i < 360; i++) {
+    if(i%10 == 0){
+        var bambu =  new fbx('./non-player asset/Park1/Bamboo/', 'BlackBamboo.fbx', 0.03, 0.03, 0.03, vertices[i*3], vertices[i*3+1], vertices[i*3+2], 0, 0, 0, Main.scene);
+    }
+}
+
+function animate() {
     Main.render(clock.getDelta());
     requestAnimationFrame(animate);
 }
